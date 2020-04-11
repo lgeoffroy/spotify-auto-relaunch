f160fd6eab60e836874025be201130a830a18ea6
ludovic geoffroy <ludovic.geoffroy@thetribe.io>
2020-04-11 18:17:07 +0200

    create auto skip add script

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # spotify-auto-relaunch
+
+Just generate an authorization code from here: https://developer.spotify.com/console/get-users-currently-playing-track/?market=&additional_types=
+
+Put it in .env as SPOTIFY_AUTHORIZATION_CODE and you're done!
+
+then `node index.js`.
+
+```
+lgeoffroy@laptop:~/spotify-auto-relaunch[master]$ node index.js
+4/11/2020, 6:25:01 PM: playing a track
+Jahneration - Gone For Long (1:54 / 3:17)
+Sleeping for 2:24.
+
+4/11/2020, 6:27:25 PM: playing a ad
+Nothing playing.
+4/11/2020, 6:27:41 PM: playing a track
+Jahneration - Cease Fire (0:11 / 5:27)
+Sleeping for 5:17.
+```
+
+### TODO:
+
+- Get authorization code automatically
+- Wait for spotify to be ready instead of waiting a few seconds
+- Hide/minimize spotify on relaunch
+- Better handle errors
+- Completely rewrite using only dbus...
---
+++ index.js
@@ -0,0 +1,68 @@
+const { exec } = require('child_process');
+const dotenv = require('dotenv');
+const fetch = require("node-fetch");
+
+dotenv.config();
+
+const getCurrentTrackInfos = () => fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${process.env.SPOTIFY_AUTHORIZATION_CODE}`,
+  },
+})
+  .then((response) => response.json());
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const play = () => exec('/usr/bin/dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play');
+const relaunch = () => exec('/usr/bin/killall spotify && /usr/bin/spotify');
+
+const skipAd = async () => {
+  relaunch();
+  await sleep(5000);
+  play();
+}
+
+const displayTime = (ms) => {
+  const s = (ms / 1000).toFixed(0);
+  const m = s / 60;
+
+  return `${m.toFixed(0)}:${`${s % 60}`.padStart(2, '0')}`;
+};
+
+const main = async () => {
+  try {
+    const trackInfos = await getCurrentTrackInfos();
+
+    const isTrack = trackInfos.currently_playing_type === 'track';
+    console.log(`${(new Date()).toLocaleString()}: playing a ${trackInfos.currently_playing_type}`);
+
+    if (isTrack) {
+      console.log(`${trackInfos.item.artists[0].name} - ${trackInfos.item.name} (${displayTime(trackInfos.progress_ms)} / ${displayTime(trackInfos.item.duration_ms)})`)
+      const timer = trackInfos.item.duration_ms - trackInfos.progress_ms + 1000; // Add 1s delay to avoid fetching too much time at the end of the song.
+      console.log(`Sleeping for ${displayTime(timer)}.\n`);
+      await sleep(timer);
+    } else {
+      await skipAd();
+    }
+  } catch (error) {
+    if (error.type === 'invalid-json') {
+      console.log('Nothing playing.');
+      await sleep(10000); // 10s
+    } else {
+      console.error(error);
+      console.log('Seems like the code is invalid. Please regenerate it here:');
+      console.log('https://developer.spotify.com/console/get-users-currently-playing-track/?market=&additional_types=');
+      await sleep(180000); // 3mn
+    }
+  }
+};
+
+const loop = async () => {
+  while (true) {
+    await main();
+  }
+};
+
+loop();
