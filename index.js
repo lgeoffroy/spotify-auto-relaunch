2e603b3e7f2db56b5a4226c134e4a9dc697dad01
ludovic geoffroy <ludovic.geoffroy@thetribe.io>
2020-10-05 21:50:53 +0200

    add slack status integration

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const dotenv = require('dotenv');
 const SpotifyApi = require('./api');
-const { displayTime, skipAd, sleep } = require('./utils');
+const { displayTime, skipAd, sleep, updateSlackStatus } = require('./utils');
 
 dotenv.config();
 
 const api = new SpotifyApi();
 
 const main = async () => {
+  let isAd = true;
   try {
     let trackInfos = await api.getCurrentTrackInfos();
 
@@ -15,29 +16,37 @@ const main = async () => {
       trackInfos = await api.getCurrentTrackInfos();
     }
 
-    const isTrack = trackInfos.currently_playing_type === 'track';
+    isAd = trackInfos.currently_playing_type === 'ad';
     console.log(`${(new Date()).toLocaleString('fr')}: playing a ${trackInfos.currently_playing_type}`);
 
-    if (isTrack) {
+    if (isAd) {
+      await skipAd();
+    } else {
       console.log(`${trackInfos.item.artists[0].name} - ${trackInfos.item.name} (${displayTime(trackInfos.progress_ms)} / ${displayTime(trackInfos.item.duration_ms)})`)
       const timer = trackInfos.item.duration_ms - trackInfos.progress_ms + 1000; // Add 1s delay to avoid fetching too much time at the end of the song.
       console.log(`Sleeping for ${displayTime(timer)}.\n`);
+      if (process.env.SLACK_API_TOKEN) {
+        await updateSlackStatus({
+          status_text: `${trackInfos.item.artists[0].name} - ${trackInfos.item.name}`,
+          status_emoji: ':musical_note:',
+          status_expiration: Math.round((Date.now() + timer) / 1000 + 60), // status will be override; add a minute to avoid time synchronization problems
+        });
+      }
       await sleep(timer);
-    } else {
-      await skipAd();
     }
   } catch (error) {
     if (error.type === 'invalid-json') {
       console.log('Nothing playing.');
-      await sleep(60 * 1000); // Nothing playing, wait 1 mn before retrying.
+      await sleep((isAd ? 2 : 60) * 1000);
     } else {
-      console.log(error);
+      console.error(error);
       process.exit(1)
     }
   }
 };
 
 const loop = async () => {
+  await skipAd();
   while (true) {
     await main();
   }
--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const { exec } = require('child_process');
+const fetch = require('node-fetch');
 
 const getEncodedCredentials = () => {
   const creds = `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`;
@@ -13,11 +14,11 @@ const navigate = async (page, url) => Promise.all([
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const play = () => exec('/usr/bin/dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play');
-const relaunchSpotify = () => exec('/usr/bin/killall spotify && /bin/sleep 1 && /usr/share/spotify/spotify &');
+const relaunchSpotify = () => exec('/usr/bin/killall spotify; /bin/sleep 1; /usr/share/spotify/spotify &');
 const minimize = () => exec('/usr/bin/xdotool search --onlyvisible --classname "spotify" windowminimize');
 
 const skipAd = async () => {
-  await sleep(2000);
+  await sleep(500);
   relaunchSpotify();
   await sleep(3000);
   play();
@@ -31,10 +32,23 @@ const displayTime = (ms) => {
   return `${m.toFixed(0)}:${`${s % 60}`.padStart(2, '0')}`;
 };
 
+const updateSlackStatus = async (profile) => {
+  const body = JSON.stringify({ profile });
+  await fetch(`https://slack.com/api/users.profile.set`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_API_TOKEN}`,
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body,
+  });
+};
+
 module.exports = {
   displayTime,
   getEncodedCredentials,
   navigate,
   skipAd,
   sleep,
+  updateSlackStatus,
 };
